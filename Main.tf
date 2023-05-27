9983218972b999673bf1a215642e68fe1fa08425
Enoch Sasu Edwards <enoch.s.edwards@gmail.com>
2023-05-27 15:56:25 +0100

    added the public and private subnet

--- Main.tf
+++ Main.tf
@@ -10,3 +10,28 @@ resource "aws_vpc" "Practice-vpc" {
     Name = "Practice-vpc"
   }
 }
+
+
+#Creating Public Subnet
+resource "aws_subnet" "public-sub-1" {
+  vpc_id     = aws_vpc.Practice-vpc.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "public-sub-1"
+  }
+}
+
+#Creating Private Subnet
+resource "aws_subnet" "private-sub-1" {
+  vpc_id     = aws_vpc.Practice-vpc.id
+  cidr_block = "10.0.11.0/24"
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "private-sub-1"
+  }
+}
+
+
